eb2ce2089ccb27239e87396e8f9d82419fa5ed30
Alexandru Bordei <alexandru.bordei@bigstep.com>
2019-04-18 14:40:06 +0300

    A different program version

--- myapp/my_test_program.py
+++ myapp/my_test_program.py
@@ -1,23 +1,23 @@
 from os import getenv
-import sys
-from socket import gethostbyname, gethostname 
-from pyspark.sql import SparkSession
+from socket import gethostbyname, gethostname
+from pyspark.sql import SparkSession,Row
 from pyspark.mllib.random import RandomRDDs
 
 SPARK_MASTER= getenv('SPARK_MASTER','local[2]')
-SIZE_X = int(getenv('SIZE_X',10))
-SIZE_Y = int(getenv('SIZE_Y',10))
 OUTPUT_DIR=getenv('OUTPUT_DIR')
-
 if(OUTPUT_DIR==None):
-	raise Exception('OUTPUT_DIR cannot be null');
+        raise Exception('OUTPUT_DIR cannot be null');
 
 spark = SparkSession.builder \
-	.appName("my_app") \
-	.config("spark.driver.host",gethostbyname(gethostname())) \
-	.master(SPARK_MASTER) \
-	.getOrCreate()
+        .appName("my_app") \
+        .config("spark.driver.host",gethostbyname(gethostname())) \
+        .master(SPARK_MASTER) \
+        .getOrCreate()
 
-data  = RandomRDDs.uniformVectorRDD(spark.sparkContext, SIZE_X, SIZE_Y).map(lambda a : a.tolist()).toDF()
+l=[('alex',25),('cristina',22),('sergiu',20),('john',26)]
+rdd = spark.sparkContext.parallelize(l)
+people = rdd.map(lambda x: Row(name=x[0], age=int(x[1])))
+
+data = spark.createDataFrame(people)
 
 data.write.mode("overwrite").save(OUTPUT_DIR);
